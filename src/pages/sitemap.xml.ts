1c0baffc1eb9433407339efa704f656652baa1b1
Sean <sean@edugamehq.com>
2025-10-09 22:09:49 +0800

    fix: 修复sitemap.xml.ts导入路径 - 将../../utils/改为../utils/ - 确保构建成功

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,5 +1,5 @@
 // 英文版sitemap.xml文件
-import { generateSitemapForLanguage } from '../../utils/sitemap-generator';
+import { generateSitemapForLanguage } from '../utils/sitemap-generator';
 
 export async function GET() {
   return generateSitemapForLanguage('en');
